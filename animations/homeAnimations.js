aed0836c62fbb3f011e2141cac65f688f6e467a6
Luis Castillo <lcastillov123@gmail.com>
2021-12-28 11:20:08 -0500

    animaciones

---
+++ animations/homeAnimations.js
@@ -0,0 +1,16 @@
+export const commonAnimation = {
+	visible: { opacity: 1, scale: 1, transition: { duration: 0.8 } },
+	hidden: { opacity: 0, scale: 0.9 },
+};
+export const titleAnimation = {
+	visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+	hidden: { opacity: 0, x: -100 },
+};
+export const logoAnimation = {
+	visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+	hidden: { opacity: 0, x: 100 },
+};
+export const socialNetworksAnimation = {
+	visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.5 } },
+	hidden: { opacity: 0, y: 100 },
+};
